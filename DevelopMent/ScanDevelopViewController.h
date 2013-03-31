4d5e75b604ec5d047f2683fffd05c75a803bcb47
chenguang <cgse.work@hotmail.com>
2013-03-31 19:46:20 +0800

    注释

--- DevelopMent/ScanDevelopViewController.h
+++ DevelopMent/ScanDevelopViewController.h
@@ -22,8 +22,8 @@
     NSMutableArray *allListArray;
 
     
-    //UITableView *searchtable;
-    PullTableView *searchtable;
+    UITableView *searchtable;
+    //PullTableView *searchtable;
     UIView *showCityView;
     UIView *showLevelView;
     UIView *showIndustryView;
@@ -64,8 +64,8 @@
 @property(nonatomic,strong)NSMutableArray *listarray3;
 @property(nonatomic,strong)NSMutableArray *listarray4;
 @property(nonatomic,strong)NSMutableArray *listarray5;
-//@property(nonatomic,strong)UITableView *searchtable;
-@property(nonatomic,strong)PullTableView *searchtable;
+@property(nonatomic,strong)UITableView *searchtable;
+//@property(nonatomic,strong)PullTableView *searchtable;
 @property(nonatomic,strong)UIActivityIndicatorView *assAiv;
 @property(nonatomic,strong)UIView *showCityView;
 @property(nonatomic,strong)UIView *showLevelView;

4b520cfb246fa58517542461a2a5568f01ab240e
rongfangzhi <rongfangzhi@gmail.com>
2013-04-01 16:50:16 +0800

    pagecontroll

--- DevelopMent/GrayPageControl.h
+++ DevelopMent/GrayPageControl.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface GrayPageControl : UIPageControl
-
+{
+    UIImage* activeImage;
+    UIImage* inactiveImage;
+}
 @end
--- DevelopMent/GrayPageControl.m
+++ DevelopMent/GrayPageControl.m
@@ -14,18 +14,26 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
-    }
+        activeImage = [[UIImage imageNamed:@"active_page_image"] retain];
+        inactiveImage = [[UIImage imageNamed:@"inactive_page_image"] retain];    }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
+-(void) updateDots
 {
-    // Drawing code
+    for (int i = 0; i < [self.subviews count]; i++)
+    {
+        UIImageView* dot = [self.subviews objectAtIndex:i];
+        if (i == self.currentPage) dot.image = activeImage;
+        else dot.image = inactiveImage;
+    }
 }
-*/
+
+-(void) setCurrentPage:(NSInteger)page
+{
+    [super setCurrentPage:page];
+    [self updateDots];
+}
+
 
 @end

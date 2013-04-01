3b010ca74300c9b7de2c3dceea80e4ef801a1b88
rongfangzhi <rongfangzhi@gmail.com>
2013-04-01 16:43:59 +0800

    pagecontroll photo

---
+++ DevelopMent/GrayPageControl.h
@@ -0,0 +1,13 @@
+//
+//  GrayPageControl.h
+//  DevelopMent
+//
+//  Created by 容芳志 on 13-4-1.
+//  Copyright (c) 2013年 xin wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GrayPageControl : UIPageControl
+
+@end
---
+++ DevelopMent/GrayPageControl.m
@@ -0,0 +1,31 @@
+//
+//  GrayPageControl.m
+//  DevelopMent
+//
+//  Created by 容芳志 on 13-4-1.
+//  Copyright (c) 2013年 xin wang. All rights reserved.
+//
+
+#import "GrayPageControl.h"
+
+@implementation GrayPageControl
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end

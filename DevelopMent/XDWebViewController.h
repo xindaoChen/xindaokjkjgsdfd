5a685fd6b6c00ca50076543d95996a61fb82c6f1
rongfangzhi <rongfangzhi@gmail.com>
2013-04-07 11:22:30 +0800

    add web

---
+++ DevelopMent/XDWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  XDWebViewController.h
+//  DevelopMent
+//
+//  Created by 容芳志 on 13-4-7.
+//  Copyright (c) 2013年 xin wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XDWebViewController : UIViewController
+
+@end
---
+++ DevelopMent/XDWebViewController.m
@@ -0,0 +1,38 @@
+//
+//  XDWebViewController.m
+//  DevelopMent
+//
+//  Created by 容芳志 on 13-4-7.
+//  Copyright (c) 2013年 xin wang. All rights reserved.
+//
+
+#import "XDWebViewController.h"
+
+@interface XDWebViewController ()
+
+@end
+
+@implementation XDWebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
